349a3cd5c02af6eb61d01b6e9ceeed2a84476244
Alejandro Venegas <alejandro.b.venegas.ch@gmail.com>
2021-09-29 17:04:05 -0300

    update

--- parte_1_exploracion_mysql.sql
+++ parte_1_exploracion_mysql.sql
@@ -1,3 +1,22 @@
+-- 1.- Definir los siguentes conceptos:
+
+-- 1.1.- Base de Datos: es una colección organizada de información estructurada.
+-- 1.2.- Tabla: es la composición de datos entre columnas y filas
+-- 1.3.- Registro:  se conoce como registro de  datos al bloque con información que forma parte de una tabla.
+-- 1.4.- Columna: representa una categoría de información en el cual contienen propiedades de informacion. Ejemplo : clientes (categoria de informacion)  nombre , apellido, rut (propiedades de clientes)
+-- 1.5.- Llave Primaria: un identificador único, información que no puede ser repetida en las propiedades del campo.
+-- 1.6.- Llave Foranea: referencia de una llave en otra tabla.
+
+-- 1.7.- Respondan: ¿Cuál es la relación entre una llave primaria y una llave foránea?
+
+-- Que la llave primaria es un identificador (valor) unico y la llave foranea  hace referencia o relacion de una tabla a otra. 
+
+CREATE USER 'explorador'@'localhost' IDENTIFIED BY 'password';
+
+
+
+GRANT ALL PRIVILEGES ON word.* TO 'explorador'@'localhost';
+
 SHOW DATABASES;
 
 USE world;
@@ -6,15 +25,40 @@ SHOW TABLES;
 
 SELECT * FROM city;
 
+-- Nombre de tabla : city  
+-- cantidad de registro : 4079 filas
+
 SELECT * FROM country;
+
+-- nombre de tabla: country
+-- cantidad de registro: 239 filas
 
 SELECT * FROM countrylanguaje;
 
+-- nombre de tabla: countrylanguage
+-- cantidad de registro: 984 filas
+
+-- 3.- Usando como datos de referencia la base de datos “world” presente en MySQL Workbench, contesta las siguientes preguntas:
+
+-- ¿Cuántas ciudades existen en la base de datos?
+
 SELECT name FROM city;
+
+-- 4079 cuidades
+
+-- Si se ordenan los países de acuerdo con su nombre ¿cuál sería el primero y cuál el último?
 
 SELECT name FROM country;
 
+-- Afghanistan es el primer pais
+-- Zimbabwe es el ultimo pais
+
+-- Tomando en cuenta todos los países de Sudamérica ¿cuántas ciudades existen entre todos ellos?
+
 SELECT city.name,country.name, country.continent FROM world.city, world.country WHERE country.continent  = 'South America';
+
+-- 57106 cuidades
+
 
 
 
--- parte_2_crear_bd.sql
+++ parte_2_crear_bd.sql
@@ -15,7 +15,7 @@ CREATE TABLE USUARIOS (
   nombre VARCHAR(20),
   apellido VARCHAR(20),
   correo_electronico VARCHAR(40),
-  telefono INT(11)
+  telefono INT
 );
 
 CREATE TABLE CAPACITACION (
@@ -23,6 +23,78 @@ CREATE TABLE CAPACITACION (
   nombre VARCHAR(20),
   horario DATETIME
 );
+
+
+INSERT INTO OPERADORES (RUN, nombre, apellido, direccion, correo_electronico, fecha_creacion_cuenta) 
+VALUES ('17101980-k', 'alejandro', 'venegas', 'pasaje quintero 3665', 'asdddada@adaaasdad.com', '2021-09-19'),
+        ('17101981-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101982-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101983-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101984-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101985-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101986-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101987-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101988-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10'),
+        ('14101989-K', 'felipe', 'polo', 'tres maria', 'jejeje@jejeje.com', '2021-10-10');
+
+INSERT INTO USUARIOS VALUES (0,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09'),
+                            (null,'alejandro', 'araya', 'jojojo@jojojo.com', '133', '2022-10-09');
+
+INSERT INTO CAPACITACION VALUES (0, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00'),
+                                (null, 'alejandro', '2022-03-09 10:30:00');
+
+USE sakila;
+
+¿Cuántas películas están registradas?
+
+SELECT * FROM film;
+
+1000 peliculas registradas
+
+¿Cuántos clientes existen en la base de datos? 
+
+SELECT * FROM customer;
+
+599 clientes
+
+¿Cuántos títulos existen en inventario? 
+
+SELECT count(sakila.inventory.film_id) FROM sakila.inventory;
+
+4581 TITULOS EN inventario
+
+¿Cuál es la película más arrendada?
+
+SELECT COUNT(sakila.rental.inventory_id) as CuentaId , inventory.film_id, sakila.film.title
+FROM sakila.rental, sakila.inventory, sakila.film 
+WHERE rental.inventory_id =  inventory.inventory_id
+AND inventory.film_id = sakila.film.film_id
+group by inventory.film_id, sakila.film.title 
+order by CuentaId DESC;
+
+
+
+'34', '103', 'BUCKET BROTHERHOOD' es la pelicula mas arrendada
+
+
+
+
 
 
 
